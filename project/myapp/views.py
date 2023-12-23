fff5e13228b93f3587f6c41612ba6701b86e4757
Avishek Saha <avisheksaha123@gmail.com>
2023-12-22 20:11:16 -0800

    static and models

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import About
 
 # Create your views here.
 def index(request):
@@ -12,7 +13,23 @@ def index(request):
     # }
     # return render(request, 'index.html', {'name': name})
     # return render(request, 'index.html', context)
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    about_1 = About()
+    about_1.name = "Avishek"
+    about_1.details = "I am a data scientist"
+    about_1.phone = 1234
+    about_1.email = "as@gmail.com"
+
+    about_2 = About()
+    about_2.name = "Saha"
+    about_2.details = "I am a data analyst"
+    about_2.phone = 1234
+    about_2.email = "saha@gmail.com"
+
+    abouts = [about_1, about_2]
+
+    # return render(request, 'index.html', {'about1': about_1, 'about2': about_2})
+    return render(request, 'index.html', {'abouts': abouts})
 
 # Counter for GET method
 # def counter(request):
